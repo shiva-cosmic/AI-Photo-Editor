063390f06c3a0317b0e4f511ea124dca6ad38f6e
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-11 18:11:16 +0530

    fdd

--- convex/schema.js
+++ convex/schema.js
@@ -2,3 +2,7 @@ users: defineTable({
   name: v.string(),
   tokenIdentifier: v.string(),
 }).index("by_token", ["tokenIdentifier"]);
+users: defineTable({
+  name: v.string(),
+  tokenIdentifier: v.string(),
+}).index("by_token", ["tokenIdentifier"]);
