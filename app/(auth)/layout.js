c6a6accda64e96dc5e4b5865fc73bf4cedd0ddc2
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-17 18:41:49 +0530

    Update

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -3,5 +3,8 @@ import React from "react";
 const AuthLayout = ({ children }) => {
   return <div className="flex justify-center pt-48">{children}</div>;
 };
+const AuthLayout = ({ children }) => {
+  return <div className="flex justify-center pt-48">{children}</div>;
+};
 
 export default AuthLayout;
