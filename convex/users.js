f6f1aad885ca01b25e6639a2778dc78c5d35afbf
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-18 18:12:59 +0530

    Update

--- convex/users.js
+++ convex/users.js
@@ -63,3 +63,25 @@ export const getCurrentUser = query({
     return user;
   },
 });
+export const getCurrentUser = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const user = await ctx.db
+
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .unique();
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
+  },
+});
