4229914e48930851c3051ef5bbbaf98e60cf06ab
Shivanshh369 <shivanshhh369@gmail.com>
2025-11-03 15:57:07 +0530

    Update

--- components/ui/sonner.jsx
+++ components/ui/sonner.jsx
@@ -19,6 +19,13 @@ const Toaster = ({
           "--normal-border": "var(--border)"
         }
       }
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)"
+        }
+      }
       {...props} />
   );
 }
