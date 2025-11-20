a89ad2e1838c6f335a08a1fd0dd3f8a4883467e8
Shivanshh369 <shivanshhh369@gmail.com>
2025-11-20 09:56:45 +0530

    Updated

--- components/hero.jsx
+++ components/hero.jsx
@@ -59,6 +59,9 @@ useEffect(()=> {
                   <div className="w-3 h-3 bg-red-500 rounded-full"></div>
                   <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
                   <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+                  <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+                  <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+                  <div className="w-3 h-3 bg-green-500 rounded-full"></div>
                 </div>
                 <div className="text-gray-400 text-sm">Pixxel Pro</div>
               </div>
