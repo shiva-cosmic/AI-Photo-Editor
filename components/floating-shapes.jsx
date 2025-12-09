20a7ed693d9a3cf2b32dd07c88b2514bd699b03c
Shivanshh369 <shivanshhh369@gmail.com>
2025-12-09 18:04:07 +0530

    Completed

--- components/floating-shapes.jsx
+++ components/floating-shapes.jsx
@@ -33,6 +33,30 @@ const FloatingShapes = () => {
       position: "bottom-1/3 right-1/4",
       gradient:"from-green-400 to-cyan-500",
     },
+    {
+      id:1,
+      size:"w-72 h-72",
+      position:"top-20 left-10",
+      gradient: "from-blue-500 to-purple-600"
+    },
+    {
+      id:2,
+      size:"w-96 h-96",
+      position:"top-1/3 right-10",
+      gradient:"from-cyan-400 to-blue-500",
+    },
+    {
+      id:3,
+      size:"w-64 h-64",
+      position:"bottom-20 left-1/4",
+      gradient:"from-purple-500 to-pink-500",
+    },
+    {
+      id:4,
+      size:"w-80 h-80",
+      position: "bottom-1/3 right-1/4",
+      gradient:"from-green-400 to-cyan-500",
+    },
 
   ];
 
