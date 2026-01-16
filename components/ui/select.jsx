11ab194a72e5b98be8c63dd8b9a9f4abffa7f673
Shivanshh369 <shivanshhh369@gmail.com>
2026-01-16 13:29:59 +0530

    Update

--- components/ui/select.jsx
+++ components/ui/select.jsx
@@ -107,11 +107,6 @@ function SelectItem({
           <CheckIcon className="size-4" />
         </SelectPrimitive.ItemIndicator>
       </span>
-      <span className="absolute right-2 flex size-3.5 items-center justify-center">
-        <SelectPrimitive.ItemIndicator>
-          <CheckIcon className="size-4" />
-        </SelectPrimitive.ItemIndicator>
-      </span>
       <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
     </SelectPrimitive.Item>
   );
