241a6b38902acafba6d74c55353d9f3416b05547
Shivanshh369 <shivanshhh369@gmail.com>
2026-01-25 12:42:20 +0530

    Slider Updated

--- components/ui/slider.jsx
+++ components/ui/slider.jsx
@@ -32,6 +32,17 @@ function Slider({
         className
       )}
       {...props}>
+    <SliderPrimitive.Root
+      data-slot="slider"
+      defaultValue={defaultValue}
+      value={value}
+      min={min}
+      max={max}
+      className={cn(
+        "relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col",
+        className
+      )}
+      {...props}>
       <SliderPrimitive.Track
         data-slot="slider-track"
         className={cn(
