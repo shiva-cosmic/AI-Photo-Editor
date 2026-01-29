ffca27c04f39aa7b60ccb28324ba18cd43430d40
Shivanshh369 <shivanshhh369@gmail.com>
2026-01-29 20:48:46 +0530

    slider demo

--- components/ui/slider.jsx
+++ components/ui/slider.jsx
@@ -42,6 +42,11 @@ function Slider({
           className={cn(
             "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
           )} />
+        <SliderPrimitive.Range
+          data-slot="slider-range"
+          className={cn(
+            "bg-primary absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full"
+          )} />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
